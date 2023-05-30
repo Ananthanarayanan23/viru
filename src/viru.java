3e7d28bc294f4de43cd4c250fd3d18600c5e1544
Anandh P <ananthanarayanan2327@gmail.com>
2023-05-31 01:35:32 +0530

    first commit

---
+++ src/viru.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Anandh
+ *
+ */
+public class viru {
+	public static void main(String args[]){  
+		String v = "anandhviru";
+		int c =0;
+		for(int i=0;i<v.length();i++){
+		    if(v.charAt(i)=='a'||v.charAt(i)=='i'){
+		        c++;
+		    
+		}
+		}
+		System.out.println("here the vowles :"+c);
+		}
+		 
+	    }  
+	
